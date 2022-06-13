a1ffd24bbe19b0a501ad92171f5e41d0b3cf37b6
WanjohiWanjohi <wamuyuwanjohi97@gmail.com>
2022-06-13 23:18:16 +0300

    commit tree 8ddccea285371e35ce01416fc9d1acaeccd51e70

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+// Your code goes here\
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    input.textContent = "This is really cool!";
+  });
+  const input = document.getElementById('text');
